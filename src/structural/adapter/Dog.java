aba1b9563844dd7d0912798c3e2ef6cd35378b88
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-04-25 10:57:28 +0900

    구조 > adapter 패턴 예제

---
+++ src/structural/adapter/Dog.java
@@ -0,0 +1,14 @@
+package structural.adapter;
+
+public class Dog extends Animal {
+
+	public Dog(String name) {
+		super(name);
+	}
+
+	@Override
+	public void sound() {
+		System.out.println(name + " Barking");
+	}
+
+}
