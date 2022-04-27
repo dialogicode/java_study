35b919a0894b4387573bded9c53dcb1364ecc054
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-04-27 16:07:58 +0900

    구조 > facade 패턴 예제

---
+++ src/structural/facade/MainUseFacade.java
@@ -0,0 +1,9 @@
+package structural.facade;
+
+public class MainUseFacade {
+	public static void main(String[] args) {
+		Facade facade = new Facade();
+		String name = "Dorosh";
+		facade.run(name);
+	}
+}
