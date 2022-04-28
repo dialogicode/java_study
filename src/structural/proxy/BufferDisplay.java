00f19eeb81be9dbda530365ed75234a9af845cf6
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-04-28 13:43:32 +0900

    구조 > Proxy 패턴 예제

---
+++ src/structural/proxy/BufferDisplay.java
@@ -0,0 +1,30 @@
+package structural.proxy;
+
+import java.util.ArrayList;
+
+public class BufferDisplay implements Display {
+	private final ArrayList<String> buffer = new ArrayList<>();
+	private final int bufferSize;
+	private ScreenDisplay screen;
+
+	public BufferDisplay(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+	@Override
+	public void print(String content) {
+		buffer.add(content);
+		if (buffer.size() == bufferSize) {
+			flush();
+		}
+	}
+
+	public void flush() {
+		if (screen == null) {
+			screen = new ScreenDisplay();
+		}
+		String lines = String.join("\n", buffer);
+		screen.print(lines);
+		buffer.clear();
+	}
+}
