dcba500a0494c3ea8f5567d85532cde764564b54
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-04-28 15:35:00 +0900

    행위 > observer 패턴 예제

---
+++ src/behavioural/observer/Main.java
@@ -0,0 +1,20 @@
+package behavioural.observer;
+
+public class Main {
+	public static void main(String[] args) {
+		DiceGame diceGame = new FairDiceGame();
+
+		Player player1 = new EvenBettingPlayer("짝궁뎅이");
+		Player player2 = new OddBettingPlayer("홀아비");
+		Player player3 = new OddBettingPlayer("홀쭉이");
+
+		diceGame.addPlayer(player1);
+		diceGame.addPlayer(player2);
+		diceGame.addPlayer(player3);
+
+		for (int i = 0; i < 5; i++) {
+			diceGame.play();
+			System.out.println();
+		}
+	}
+}
---
+++ src/behavioural/observer/Player.java
@@ -0,0 +1,15 @@
+package behavioural.observer;
+
+public abstract class Player {
+	private final String name;
+
+	public Player(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public abstract void update(int diceNumber);
+}
