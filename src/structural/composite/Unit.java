561bc0ad66acb14f79caa284cf8801a00463df71
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-04-28 14:17:58 +0900

    구조 > composite 패턴 예제

---
+++ src/structural/composite/Unit.java
@@ -0,0 +1,20 @@
+package structural.composite;
+
+public abstract class Unit {
+	private final String name;
+
+	public Unit(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name + "(" + getSize() + ")";
+	}
+
+	public abstract int getSize();
+}
