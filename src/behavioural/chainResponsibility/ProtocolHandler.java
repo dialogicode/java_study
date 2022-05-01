4b96c30c26bfe76024405ad63606e7847e8099a9
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-05-01 22:52:21 +0900

    행위 > chain of Responsibility 패턴 예제

---
+++ src/behavioural/chainResponsibility/ProtocolHandler.java
@@ -0,0 +1,13 @@
+package behavioural.chainResponsibility;
+
+public class ProtocolHandler extends Handler {
+	@Override
+	protected void process(String url) {
+		int index = url.indexOf("://");
+		if (index != -1) {
+			System.out.println("PROTOCOL: " + url.substring(0, index));
+		} else {
+			System.out.println("NO PROTOCOL");
+		}
+	}
+}
