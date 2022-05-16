29030ed11191f37e60648c2d46891d258a534d82
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-05-16 21:31:05 +0900

    unit1 lession1.2

--- android_fundamentals/app/src/main/java/me/study/unit1/Lesson11_HelloLog.java
+++ android_fundamentals/app/src/main/java/me/study/unit1/Lesson11_HelloLog.java
@@ -14,24 +14,15 @@ public class Lesson11_HelloLog extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lesson11);
 
-		// part 01.1
-		// 로그 명령문 출력: 필터링 설정 d-debug, e-error, w-warn, i-info
-		// 첫번째 매개변수 : 로그태그
-			// 로그태그 상수 > Activity 예시
-			// private static final String LOG_TAG = MainActivity.class.getSimpleName();
-		// 두번째 매개변수 : 메세지
-		Log.d("MainActivity", "Hello World");
+		/*  part 01.1
+		 *
+		 *  로그 명령문 출력: 필터링 설정 d-debug, e-error, w-warn, i-info
+		 *  첫번째 매개변수 : 로그태그
+		 *      로그태그 상수 > Activity 예시
+		 *      private static final String LOG_TAG = MainActivity.class.getSimpleName();
+		 *  두번째 매개변수 : 메세지
+		 */
 
-		/*  part 01.2
-		*
-		* 	안드로이드 기기 화면의 UI - View 라는 객체 계층으로 구성
-		*   TextViw     : display text
-		*   EditText    : enable to enter and edit text
-		*   Button, RadioButton, CheckBox, Spinner  : clickable element
-		*   ScrollView, RecyclerView                : scrollable item
-		*   ImageView   : display image
-		*
-		*   ConstraintLayout, LinearLayout  : containing other View element and positioning them.
-		*/
+		Log.d("MainActivity", "Hello World");
 	}
 }
--- android_fundamentals/app/src/main/java/me/study/unit1/Lesson12_HelloToast.java
+++ android_fundamentals/app/src/main/java/me/study/unit1/Lesson12_HelloToast.java
@@ -3,14 +3,47 @@ package me.study.unit1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import me.study.R;
 
 public class Lesson12_HelloToast extends AppCompatActivity {
+	private int mCount = 0;
+	private TextView mShowCount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lesson12);
+
+		/*  part 01.2
+		 *
+		 * 	안드로이드 기기 화면의 UI - View 라는 객체 계층으로 구성
+		 *  TextViw     : display text
+		 *  EditText    : enable to enter and edit text
+		 *  Button, RadioButton, CheckBox, Spinner  : clickable element
+		 *  ScrollView, RecyclerView                : scrollable item
+		 *  ImageView   : display image
+		 *
+		 *  ConstraintLayout, LinearLayout  : containing other View element and positioning them
+		 */
+
+		mShowCount = (TextView) findViewById(R.id.show_count);
+
+	}
+
+	public void showToast(View view) {
+		// 짧게 화면에 팝업되서 나타나는 메세지 : Toast
+		Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	public void countUp(View view) {
+		mCount++;
+		if (mShowCount != null) {
+			mShowCount.setText(Integer.toString(mCount));
+		}
 	}
 }
